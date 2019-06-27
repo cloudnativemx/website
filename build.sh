810162127f198d10ebf3d30c4fd32a6c92afaf7a
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-06-27 17:56:03 -0500

    bump version to 0.0.4

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-VER=$(grep "version" website.properties|cut -d'=' -f2)  && docker build -t cloudnativemx/website:$VER . #&& docker push cloudnativemx/website:$VER #&& kubectl set image deployment/website website=cloudnativemx/website:$VER'
+VER=$(grep "version" website.properties|cut -d'=' -f2)  && docker build -t cloudnativemx/website:$VER . && docker push cloudnativemx/website:$VER #&& kubectl set image deployment/website website=cloudnativemx/website:$VER'

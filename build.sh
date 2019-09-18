701983ee8b57e61090c9d60e47fffb58d7e8955a
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-09-18 15:28:18 -0500

    enhanced stuff

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-VER=$(grep "version" website.properties|cut -d'=' -f2)  && docker build -t cloudnativemx/website:latest -t cloudnativemx/website:$VER . && \
-#echo "fin" 
-docker push cloudnativemx/website:$VER && docker push cloudnativemx/website:latest && git release $VER
+VER=$(grep "version" website.properties|cut -d'=' -f2)  && \
+  docker build -t cloudnativemx/website:latest -t cloudnativemx/website:$VER . && \
+  git release $VER && \
+  docker push cloudnativemx/website:$VER && docker push cloudnativemx/website:latest

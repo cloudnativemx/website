c10ad8fd526dd1c36b53cb239ea13d06c02289a1
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-03-23 12:02:23 -0600

    code cleanup

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 VER=$(grep "version" website.properties|cut -d'=' -f2)  && \
-  docker build -t cloudnativemx/website:latest -t cloudnativemx/website:$VER -t registry.circulosiete.com/library/cloudnativemx/website:$VER . && \
+  docker build -t cloudnativemx/website:latest \
+    -t cloudnativemx/website:$VER \
+    -t registry.circulosiete.com/library/cloudnativemx/website:$VER . && \
   git release $VER && \
-  docker push cloudnativemx/website:$VER && docker push cloudnativemx/website:latest && docker push registry.circulosiete.com/library/cloudnativemx/website:$VER
+  docker push cloudnativemx/website:$VER && 
+    docker push cloudnativemx/website:latest && 
+    docker push registry.circulosiete.com/library/cloudnativemx/website:$VER
